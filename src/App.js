8788c1b1fa6669cc9e5202ac5bf05c78f9942f79
ElDav1d <dvivoes@yahoo.es>
2023-03-10 10:02:33 +0100

    add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
