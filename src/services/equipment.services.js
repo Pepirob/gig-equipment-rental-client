7e61e51fb07b9c2c9c39126da6bc344d9f02d7fe
Jose Luis <pepirob>
2023-03-11 20:39:54 +0100

    equipment details service create

--- src/services/equipment.services.js
+++ src/services/equipment.services.js
@@ -4,4 +4,8 @@ const myEquipmentService = () => {
   return service.get("/equipment/my-equipment");
 };
 
-export { myEquipmentService };
+const equipmentDetailsService = (equipId) => {
+  return service.get(`/equipment/${equipId}`);
+};
+
+export { myEquipmentService, equipmentDetailsService };
