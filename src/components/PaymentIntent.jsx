85c80678cc86cf90f52206d49a2d3c2dfd3e3fbf
ElDav1d <dvivoes@yahoo.es>
2023-03-12 18:33:58 +0100

    create payment intent

---
+++ src/components/PaymentIntent.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import { createPaymentIntentService } from "../services/payment.services";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+
+function PaymentIntent({ productDetails }) {
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    handleUseEffect();
+  }, []);
+
+  const handleUseEffect = async () => {
+    //                   this is the product info sent to the backend with the product to purchase
+    //                                                    |
+    const response = await createPaymentIntentService(productDetails);
+    setClientSecret(response.data.clientSecret);
+  };
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return (
+    <div className="App">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
+}
+
+export default PaymentIntent;
