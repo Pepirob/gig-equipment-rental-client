e8279e8c113d8a3ed73dd5c32443f44ae6758aa7
Jose Luis <pepirob>
2023-03-12 19:17:03 +0100

    render available equipment home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ListEquipment from "../components/ListEquipment";
 import Navigation from "../components/Navigation";
+import { availableEquipmentService } from "../services/equipment.services";
 
 function Home() {
+  const redirect = useNavigate();
+  const [availableEquipment, setAvailableEquipment] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await availableEquipmentService();
+      setAvailableEquipment(response.data);
+      setIsFetching(false);
+    } catch (error) {
+      redirect("error");
+    }
+  };
+
   return (
     <>
       <header>
         <Navigation />
       </header>
-      <main>Home</main>
+      <main>
+        <h1>Home</h1>
+        {!isFetching && <ListEquipment equipment={availableEquipment} />}
+      </main>
     </>
   );
 }
