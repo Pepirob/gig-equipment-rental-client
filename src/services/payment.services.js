6b5e8b9238d233577de54da8cfa19890bcc060bf
Jose Luis <pepirob>
2023-03-13 11:56:34 +0100

    renaming service

--- src/services/payment.services.js
+++ src/services/payment.services.js
@@ -1,7 +1,7 @@
 import service from "./config.services";
 
 const createPaymentIntentService = (productId) => {
-  return service.post("/payment/create-payment-intent", productId);
+  return service.post("/transaction/create-payment-intent", productId);
 };
 
 export { createPaymentIntentService };
