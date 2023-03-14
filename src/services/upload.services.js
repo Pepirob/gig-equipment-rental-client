1a5f6d51616bcd261bf3a68fb155745bc6f4d3f3
ElDav1d <dvivoes@yahoo.es>
2023-03-14 07:56:56 +0100

    consolidate explicit return

--- src/services/upload.services.js
+++ src/services/upload.services.js
@@ -1,8 +1,11 @@
 import service from "./config.services";
 
-const uploadEquipmentImgService = (img) =>
-  service.post("/upload/equipmentImg", img);
+const uploadEquipmentImgService = (img) => {
+  return service.post("/upload/equipmentImg", img);
+};
 
-const uploadUserImgService = (img) => service.post("/upload/userImg", img);
+const uploadUserImgService = (img) => {
+  return service.post("/upload/userImg", img);
+};
 
 export { uploadUserImgService, uploadEquipmentImgService };
