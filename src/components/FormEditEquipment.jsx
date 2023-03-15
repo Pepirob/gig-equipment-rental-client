d3d030de36d6f27c38b7050c33e7ced6c0a0ec5f
ElDav1d <dvivoes@yahoo.es>
2023-03-15 08:28:23 +0100

    protect equipment availability

--- src/components/FormEditEquipment.jsx
+++ src/components/FormEditEquipment.jsx
@@ -10,7 +10,6 @@ function FormEditEquipment({ equipmentData }) {
   const [description, setDescription] = useState("");
   const [pricePerDay, setPricePerDay] = useState(0);
   const [deposit, setDeposit] = useState(0);
-  const [isAvailable, setIsAvailable] = useState(equipmentData.isAvailable);
   const [isUploading, setIsUploading] = useState(false);
   const [isFetching, setIsFetching] = useState(false);
 
@@ -19,7 +18,6 @@ function FormEditEquipment({ equipmentData }) {
     setDescription(equipmentData.description);
     setPricePerDay(equipmentData.pricePerDay);
     setDeposit(equipmentData.deposit);
-    setIsAvailable(equipmentData.isAvailable);
   }, []);
 
   const handleFileUpload = async (event) => {
@@ -56,9 +54,6 @@ function FormEditEquipment({ equipmentData }) {
       case "deposit":
         setDeposit(value);
         break;
-      case "isAvailable":
-        setIsAvailable(!isAvailable);
-        break;
       default:
         setName(value);
     }
@@ -72,20 +67,20 @@ function FormEditEquipment({ equipmentData }) {
 
       if (imgUrl) {
         await updateEquipmentService(equipmentData._id, {
+          ...equipmentData,
           name,
           description,
           pricePerDay,
           deposit,
           img: imgUrl,
-          isAvailable,
         });
       } else {
         updateEquipmentService(equipmentData._id, {
+          ...equipmentData,
           name,
           description,
           pricePerDay,
           deposit,
-          isAvailable,
         });
       }
       setIsFetching(false);
@@ -141,16 +136,6 @@ function FormEditEquipment({ equipmentData }) {
           type="number"
           name="deposit"
           value={deposit}
-          onChange={handleInput}
-        />
-        <br />
-        <br />
-        <label htmlFor="isAvailable">Available</label>
-        <input
-          type="checkbox"
-          name="isAvailable"
-          checked={isAvailable}
-          value={isAvailable}
           onChange={handleInput}
         />
         <br />
