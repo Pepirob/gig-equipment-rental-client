ae07690ac033e45b6bc315038e4841858f018375
Jose Luis <pepirob>
2023-03-15 21:00:28 +0100

    create delivered button

--- src/pages/TransactionDetails.jsx
+++ src/pages/TransactionDetails.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import SheetTransaction from "../components/SheetTransaction";
-import { getTransactionDetailsService } from "../services/transactions.services";
+import {
+  getTransactionDetailsService,
+  updateTransactionStateService,
+} from "../services/transactions.services";
 
 function TransactionDetails() {
   const redirect = useNavigate();
   const params = useParams();
   const { transactionId } = params;
-  const [transaction, setTransaction] = useState();
+  const [transaction, setTransaction] = useState(null);
   const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
@@ -24,6 +27,21 @@ function TransactionDetails() {
     }
   };
 
+  const handleDeliveredState = async () => {
+    try {
+      setIsFetching(true);
+
+      const response = await updateTransactionStateService(transactionId, {
+        state: "delivered",
+      });
+
+      setIsFetching(false);
+      setTransaction(response.data);
+    } catch (error) {
+      redirect("/error");
+    }
+  };
+
   return (
     <>
       <header>
@@ -31,6 +49,11 @@ function TransactionDetails() {
       </header>
       <main>
         {!isFetching && <SheetTransaction transaction={transaction} />}
+        {!isFetching && transaction.state === "delivered" ? (
+          <p>Equipment delivered</p>
+        ) : (
+          <button onClick={handleDeliveredState}>Mark as delivered</button>
+        )}
       </main>
     </>
   );
