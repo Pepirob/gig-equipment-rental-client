6f0360446222cb7fcb12fc67a086c7442927cfa2
Jose Luis <pepirob>
2023-03-15 17:35:24 +0100

    Render owner in transaction details

--- src/components/SheetTransaction.jsx
+++ src/components/SheetTransaction.jsx
@@ -1,16 +1,41 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getEquipmentDetailsService } from "../services/equipment.services";
+
 function SheetTransaction({ transaction }) {
+  const redirect = useNavigate();
+  const [equipment, setEquipment] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+
   const transactionCreatedDay = new Date(transaction.updatedAt);
+
   const lastDayRent = new Date(
     transactionCreatedDay.getTime() +
       transaction.daysRented * 24 * 60 * 60 * 1000
   );
+
+  useEffect(() => {
+    getData();
+  }, []);
+  const getData = async () => {
+    try {
+      const response = await getEquipmentDetailsService(
+        transaction.equipment._id
+      );
+      setEquipment(response.data);
+      setIsFetching(false);
+    } catch (error) {
+      redirect("/error");
+    }
+  };
+
   return (
     <>
       <h1>{transaction.equipment.name}</h1>
       <img src={transaction.equipment.img} width="100" />
-      <p>{transaction.equipment.pricePerDay}</p>
-      <p>{transaction.equipment.deposit}</p>
-      <p>{transaction.equipment.owner}</p>
+      <p>Price per Day: {transaction.equipment.pricePerDay}</p>
+      <p>Deposit: {transaction.equipment.deposit}</p>
+      {!isFetching && <Link to="">{equipment.owner.username}</Link>}
 
       <p>
         Periodo de alquiler:{" "}
