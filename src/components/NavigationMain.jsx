903bb54975e904d138281c904b9b6ac6351a5458
ElDav1d <dvivoes@yahoo.es>
2023-03-16 20:51:11 +0100

    add navigation CTA

--- src/components/NavigationMain.jsx
+++ src/components/NavigationMain.jsx
@@ -2,9 +2,19 @@ import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
 import NavBar from "./NavBar/NavBar";
 import NavItem from "./NavItem/NavItem";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 function Navigation() {
+  const redirect = useNavigate();
   const { isLoggedIn } = useContext(AuthContext);
+
+  const handlePublish = (event) => {
+    event.preventDefault();
+    redirect("/create-equipment");
+  };
+
   return (
     <NavBar>
       <NavItem path="/">Home</NavItem>
@@ -14,7 +24,11 @@ function Navigation() {
           <NavItem path="/login">Login</NavItem>
         </>
       )}
-      <NavItem path="/create-equipment">Publish your Equipment</NavItem>
+      <Nav.Item as="li">
+        <Button as="NavLink" variant="success" onClick={handlePublish}>
+          Rent your Equipment
+        </Button>
+      </Nav.Item>
       {isLoggedIn && (
         <>
           <NavItem path="/dashboard">avatar lleva a dashboard</NavItem>
