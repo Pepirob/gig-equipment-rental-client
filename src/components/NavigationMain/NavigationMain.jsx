bcb2299d8f9a911f4b5239220b0050fb8f9e2dd0
ElDav1d <dvivoes@yahoo.es>
2023-03-16 19:09:32 +0100

    crete main navigation

---
+++ src/components/NavigationMain/NavigationMain.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth.context";
+import NavBar from "../NavBar/NavBar";
+import NavItem from "../NavItem/NavItem";
+
+NavItem;
+function Navigation() {
+  const { isLoggedIn } = useContext(AuthContext);
+  return (
+    <NavBar>
+      <NavItem path="/">Home</NavItem>
+      {!isLoggedIn && (
+        <>
+          <NavItem path="/register">Register</NavItem>
+          <NavItem path="/login">Login</NavItem>
+        </>
+      )}
+      <NavItem path="/create-equipment">Publish your Equipment</NavItem>
+      {isLoggedIn && (
+        <>
+          <NavItem path="/dashboard">avatar lleva a dashboard</NavItem>
+        </>
+      )}
+    </NavBar>
+  );
+}
+
+export default Navigation;
