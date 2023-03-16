8817851bee7156615204fa621b2dca52bdd864b5
ElDav1d <dvivoes@yahoo.es>
2023-03-16 15:48:26 +0100

    remove image inline styling

--- src/components/ItemEquipment.jsx
+++ src/components/ItemEquipment.jsx
@@ -4,7 +4,7 @@ import { capitalize } from "../utils/index";
 function ItemEquipment({ item }) {
   return (
     <li>
-      <img src={item.img} alt="equip" width="100" />
+      <img src={item.img} alt="equip" />
       <Link to={`/equipment/${item._id}`}>
         <h3>{capitalize(item.name)} </h3>
       </Link>
