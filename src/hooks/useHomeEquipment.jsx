84736c6000d5a574bd0af4506855bf4d81cbce19
Jose Luis <pepirob>
2023-06-27 18:08:09 +0200

    useRef code efitiency implementation

--- src/hooks/useHomeEquipment.jsx
+++ src/hooks/useHomeEquipment.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useDebounce from "./useDebounce";
 import {
@@ -10,15 +10,16 @@ export function useHomeEquipmentData({ searchInput }) {
   const redirect = useNavigate();
   const [availableEquipment, setAvailableEquipment] = useState(null);
   const [isFetching, setIsFetching] = useState(true);
-
+  const prevSearch = useRef(searchInput)
   useDebounce(searchInput, 500, () => getData());
 
   const getData = async () => {
+    if (searchInput === prevSearch.current) return
     try {
+      prevSearch.current = searchInput
       const response = await (searchInput
         ? getLocatedEquipmentService(searchInput)
         : getAvailableEquipmentService());
-
       setAvailableEquipment(response.data);
       setIsFetching(false);
     } catch (error) {
